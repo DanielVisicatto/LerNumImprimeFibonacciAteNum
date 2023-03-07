f43cb5396941b028eae6524c6cf619b8ef485c5c
Daniel Visicatto <dpfreestyle@gmail.com>
2023-03-06 21:34:42 -0300

    Calculo de Fibonacci de forma recursiva

--- LerNumImprimeFibonacciAteNum/Program.cs
+++ LerNumImprimeFibonacciAteNum/Program.cs
@@ -1,80 +1,37 @@
-﻿int fbnt1 = 1, fbnt2 = 1, proxFbnt = 0;
-Console.Write("Digite um número para trazer Fibonacci: ");
-if (!int.TryParse(Console.ReadLine(), out var number))
-{
-    Console.WriteLine("Inteiro por favor!");
-}
-else if (number == 1)
-{
-    Console.WriteLine("1");
-}
-else if (number == 2)
-{
-    Console.WriteLine("1, 1");
-}
-else
-{
-    Console.WriteLine("Você digitou {0}\n" +
-        "Sequência Fibonacci até {0}...", number);
+﻿int number;
+ProcessaDados();
 
-    
-    Console.Write("{0}, {1}", fbnt1, fbnt2);// imprimindo os 2 primeiros valores...
-    proxFbnt = fbnt1 + fbnt2;//calculando o proximo da sequencia...
-
-    int cont = 3;
-    //while(proxFbnt <= number)// percorrendo os fibonacci até o número...
-    while (cont <= number)
+void ProcessaDados()
+{
+    Console.Write("Digite um número para trazer Fibonacci: ");
+    if (!int.TryParse(Console.ReadLine(), out number))
     {
-        //imprimindo o proximo fibonacci...
-        //Console.Write(", {0}", proxFbnt);
-        //andando pela sequencia e imprimindo proximo...
-        TrocaValorEPegaProx();
-        Console.Write(", {0}", proxFbnt);
-        cont++;
+        Console.WriteLine("Inteiro por favor!");
+    }
+    else
+    {
+        GerarFibonacci(number);
+        ImprimirSequencia();
     }
 }
-void TrocaValorEPegaProx()
+int GerarFibonacci(int n)
 {
-    fbnt1 = fbnt2;
-    fbnt2 = proxFbnt;
-    proxFbnt = fbnt1 + fbnt2;
+    //Base 0 e 1 imutavel.
+    if (n <= 1)
+    {
+        return n;
+    }
+    //Senao retorna a soma dos dois numeros que vem antes do que foi digitado digitado n-1 e n-2
+    else
+    {
+        return GerarFibonacci(n - 1) + GerarFibonacci(n - 2);
+    }
 }
-//Console.Write("Digite um número para trazer Fibonacci: ");
-//if(!int.TryParse(Console.ReadLine(), out var number))
-//{
-//    Console.WriteLine("Inteiro por favor!");
-//}
-//else
-//{
-//    #region[comentado]
-//    //int ant = 1, prox = 0, atual = 1;
-//    //for (int i = 0; i < number; i++)
-//    //{
-//    //    atual = prox + ant;
-//    //    ant = prox;
-//    //    prox = atual;
-
-//    //    Console.Write(" {0},", prox);
-//    //}
-//    #endregion
-
-
-// com duas variáveis
-//    int anterior = 1, proximo = 0;
-//    for(int i = 0; i < number;i++)
-//    {
-//        Console.Write("{0}, ", proximo);
-//        proximo += anterior;
-//        anterior = proximo - anterior;
-//    }
-//}
-
-
-//Testando possibilidade de 1 variável...
-//int number = 1;
-//Console.Write("{0}", number);
-//for (; number <= 10; number ++)
-//{
-//    Console.Write(", {0} ", number);
-//    number+= 1;
-//}
+void ImprimirSequencia()
+{
+    //um for comum onde o incremento i será a base da chamada recursiva GerarFibonacci(i) 
+    for (int i = 0; i < number; i++)
+    {
+        Console.Write("{0}, ",GerarFibonacci(i));
+    }
+}
